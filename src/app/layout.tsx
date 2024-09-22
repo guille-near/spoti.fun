b387da5b30f62c97cc9da25b70b1ce20b49c5020
guille <guille@flowtheapp.com>
2024-09-23 00:36:59 +0100

    Precarga y notificaición 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/700.css'
-import '@/styles/globals.css'
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
+import '@/styles/globals.css';
 
 export default function RootLayout({
   children,
